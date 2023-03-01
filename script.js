f1c2a31c2cd0acdfc4c511a22cf7da90c879ab03
Andrew M <mr.mostellar@gmail.com>
2023-03-01 12:46:41 -0700

    adding more functionality and making it more mobile responsive

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 //grab the toggle box
 const toggle = document.querySelector("input[type='checkbox']");
 
-//grab the div that will house the dropdown menu
-const dropdownMenu = document.getElementById("dropdown");
-const dropdownButton = document.getElementById("dropdown-btn");
-//create a function that triggers the dropdown
-
-
 //EventListener and function to change the theme
 toggle.addEventListener("change", function(event) {
-    const sunIcon = document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun"></i>';
-    const moonIcon = document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
-    const darkTitle = document.getElementById("mode-title").innerHTML = '<span class="mode-title">Dark Mode</span>';
-    const lightTitle = document.getElementById("mode-title").innerHTML = '<span class="mode-title">Light Mode</span>';
     
     if(event.target.checked){
         document.documentElement.setAttribute("data-theme", "dark");
-        sunIcon.hidden;
-        moonIcon;
-        lightTitle.hidden;
-        darkTitle;   
+        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
+        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title">Dark Mode</span>';
     } else {
         document.documentElement.setAttribute("data-theme", "light");
-        moonIcon.hidden;
-        sunIcon;
-        darkTitle.hidden;
-        lightTitle;  
+        document.getElementById("mode-icon").innerHTML= '<i class="fa-solid fa-sun"></i>';
+        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title">Light Mode</span>';
     }
 });
+
+const dropDownMenu = document.getElementById("dropdown-item").classList;
+console.log(dropDownMenu);
+
+//create a listener that triggers the dropdown or keeps the button hidden
+document.getElementById("dropdown").addEventListener("mouseover", function(event) {
+    
+    if (event.target.change){
+        //show the dropdown
+        alert("works");
+    } else {
+
+    }
+});
+
+
+
 
 
 
