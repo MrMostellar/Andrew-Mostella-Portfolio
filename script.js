6cb1579ab76e6a66bec7baaab0d8ef3e9b879da8
Andrew M <mr.mostellar@gmail.com>
2023-03-01 04:19:07 -0700

    added more mobile compatibility

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //grab the toggle box
 const toggle = document.querySelector("input[type='checkbox']");
+//grab the div that will house the dropdown menu on small screens
+const dropdownMenu = document.getElementById("dropdown");
+
+//create a function that triggers the dropdown
+
 
 //EventListener and function to change the theme
 toggle.addEventListener("change", function(event) {
@@ -7,10 +12,16 @@ toggle.addEventListener("change", function(event) {
         document.documentElement.setAttribute("data-theme", "dark");
         document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun" hidden></i>';
         document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
+        document.getElementById("mode-title").innerHTML = '<span class="mode-title" hidden>Light Mode</span>'
+        document.getElementById("mode-title").innerHTML = '<span class="mode-title">Dark Mode</span>'
+        
     } else {
         document.documentElement.setAttribute("data-theme", "light");
         document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon" hidden></i>';
         document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun"></i>';
+        document.getElementById("mode-title").innerHTML = '<span class="mode-title" hidden>Dark Mode</span>'
+        document.getElementById("mode-title").innerHTML = '<span class="mode-title">Light Mode</span>'
+        
     }
 });
 
