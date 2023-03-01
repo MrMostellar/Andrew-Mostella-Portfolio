73365ac94ef0c3608ab2cab5322de333a39262b8
Andrew M <mr.mostellar@gmail.com>
2023-03-01 04:47:26 -0700

    Added styling to the dropdown button

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 //grab the toggle box
 const toggle = document.querySelector("input[type='checkbox']");
-//grab the div that will house the dropdown menu on small screens
-const dropdownMenu = document.getElementById("dropdown");
 
+//grab the div that will house the dropdown menu
+const dropdownMenu = document.getElementById("dropdown");
+const dropdownButton = document.getElementById("dropdown-btn");
 //create a function that triggers the dropdown
 
 
 //EventListener and function to change the theme
 toggle.addEventListener("change", function(event) {
+    const sunIcon = document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun"></i>';
+    const moonIcon = document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
+    const darkTitle = document.getElementById("mode-title").innerHTML = '<span class="mode-title">Dark Mode</span>';
+    const lightTitle = document.getElementById("mode-title").innerHTML = '<span class="mode-title">Light Mode</span>';
+    
     if(event.target.checked){
         document.documentElement.setAttribute("data-theme", "dark");
-        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun" hidden></i>';
-        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
-        document.getElementById("mode-title").innerHTML = '<span class="mode-title" hidden>Light Mode</span>'
-        document.getElementById("mode-title").innerHTML = '<span class="mode-title">Dark Mode</span>'
-        
+        sunIcon.hidden;
+        moonIcon;
+        lightTitle.hidden;
+        darkTitle;   
     } else {
         document.documentElement.setAttribute("data-theme", "light");
-        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon" hidden></i>';
-        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun"></i>';
-        document.getElementById("mode-title").innerHTML = '<span class="mode-title" hidden>Dark Mode</span>'
-        document.getElementById("mode-title").innerHTML = '<span class="mode-title">Light Mode</span>'
-        
+        moonIcon.hidden;
+        sunIcon;
+        darkTitle.hidden;
+        lightTitle;  
     }
 });
+
+
 
 
 
