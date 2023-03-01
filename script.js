47603b84141fd3b90ac7f21012eb3cdcf8b51a93
Andrew M <mr.mostellar@gmail.com>
2023-03-01 00:55:17 -0700

    Added dark and light theme compatibility with a switch in the navigation menu

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//grab the toggle box
+const toggle = document.querySelector("input[type='checkbox']");
+
+//EventListener and function to change the theme
+toggle.addEventListener("change", function(event) {
+    if(event.target.checked){
+        document.documentElement.setAttribute("data-theme", "dark");
+        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun" hidden></i>';
+        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon" hidden></i>';
+        document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }
+});
+
 
 
 
