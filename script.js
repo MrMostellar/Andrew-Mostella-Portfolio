af7f421f03e63892087a9118cdffa67f044894bc
Andrew M <mr.mostellar@gmail.com>
2023-03-01 18:04:39 -0700

    Added some titles to the dark/light theme toggle as well as styling for better mobile compatibility

--- script.js
+++ script.js
@@ -7,29 +7,51 @@ toggle.addEventListener("change", function(event) {
     if(event.target.checked){
         document.documentElement.setAttribute("data-theme", "dark");
         document.getElementById("mode-icon").innerHTML = '<i class="fa-solid fa-moon"></i>';
-        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title">Dark Mode</span>';
+        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title"title="Set to Light Theme">Dark Theme</span>';
     } else {
         document.documentElement.setAttribute("data-theme", "light");
         document.getElementById("mode-icon").innerHTML= '<i class="fa-solid fa-sun"></i>';
-        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title">Light Mode</span>';
+        document.getElementById("mode-title").innerHTML = '<span class="switch-item" id="mode-title" title="Set to Dark Theme">Light Theme</span>';
     }
 });
+//grab variables for the element and its' uses
+const dropDownActivate = document.getElementById("dropdown-btn");
+const dropDownDeactivate = document.getElementById("dropdown-btn");
 
-const dropDownMenu = document.getElementById("dropdown-item").classList;
-console.log(dropDownMenu);
+//grab variable for the dropdown-item
+const dropDownList = document.querySelector("#dropdown");
 
-//create a listener that triggers the dropdown or keeps the button hidden
-document.getElementById("dropdown").addEventListener("mouseover", function(event) {
+// create a function for opening the menu
+function openDropDownMenu(){
+
+};
+
+function closeDropDownMenu(){
+
+};
+
+//listen for a click event, mainly for mobile
+dropDownActivate.addEventListener("click", () => {
+    openDropDownMenu();
     
-    if (event.target.change){
-        //show the dropdown
-        alert("works");
-    } else {
-
-    }
 });
 
+// dropDownActivate.addEventListener("click", () => {
+//     closeDropDownMenu();
+// });
 
+//listen for a mouse over and mouse out event
+dropDownActivate.addEventListener("mouseover", (event) => {
+    if(event){
+        
+    };
+});
+
+dropDownDeactivate.addEventListener("mouseout", (event) =>{
+    if(event){
+        
+    };
+});
 
 
 
